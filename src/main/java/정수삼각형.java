ba8b59a50fb3f13bb86728d0665e6b3b4f06e043
Star-ho <58648988+Star-ho@users.noreply.github.com>
2021-09-18 09:09:55 +0900

    문제풀이

---
+++ src/main/java/정수삼각형.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class 정수삼각형 {
+    public static void main(String[] arg){
+        System.out.println(solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5} } ) );
+    }
+
+    public static int solution(int[][] triangle) {
+        int answer=0;
+
+        for(int i=1;i<triangle.length;i++){
+            loop:for(int j=0;j<triangle[i].length;j++){
+                if(j==0)triangle[i][j]=triangle[i][j]+triangle[i-1][0];
+                else if(j>0&&j<triangle[i].length-1)triangle[i][j]=triangle[i][j]+Math.max(triangle[i-1][j-1],triangle[i-1][j]);
+                else triangle[i][j]=triangle[i][j]+triangle[i-1][j-1];
+            }
+        }
+        for(int i:triangle[triangle.length-1]){
+            answer=Math.max(answer,i);
+        }
+        return answer;
+    }
+}
