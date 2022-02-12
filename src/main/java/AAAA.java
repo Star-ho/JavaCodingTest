77bc3ba942ee452e89ddf2178b2be365eb366bee
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-02-13 00:07:04 +0900

    문제풀이

---
+++ src/main/java/AAAA.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class AAAA {
+    public static void main(String[] arg){
+        System.out.println(solution(new int[]{1, 5, 2, 6, 3, 7, 4},new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}));
+    }
+
+    public static int[] solution(int[] array, int[][] commands) {
+        int i=0;
+        var a=0;
+        while(i<10){
+            try {
+                System.out.println("try block");
+                System.out.println(1/0);
+                break;
+            }catch (Exception e){
+                System.out.println("Catch block");
+                i++;
+            }
+        }
+
+        return new int[]{1, 5, 2, 6, 3, 7, 4};
+
+    }
+}
