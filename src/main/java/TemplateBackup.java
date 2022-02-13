bbf15009fdb8cf58ed461223878f7551cc4ce5aa
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-02-13 13:25:23 +0900

    템플릿

--- src/main/java/TemplateBackup.java
+++ src/main/java/TemplateBackup.java
@@ -5,13 +5,24 @@ import java.util.Arrays;
 
 public class TemplateBackup {
     public static void main(String[] arg){
+        System.out.println(solution(new int[]{1, 5, 2, 6, 3, 7, 4},new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}));
+    }
 
-        var a = LocalDateTime.now().minusMonths(3);
-        var b = LocalDateTime.now();
-        while (a.isBefore(b)){
-            System.out.println(a.format(DateTimeFormatter.ofPattern("YYYY-MM-DD")));
-            a=a.plusDays(1);
+    public static int[] solution(int[] array, int[][] commands) {
+        int i=0;
+        var a=0;
+        while(i<10){
+            try {
+                System.out.println("try block");
+                System.out.println(1/0);
+                break;
+            }catch (Exception e){
+                System.out.println("Catch block");
+                i++;
+            }
         }
+
+        return new int[]{1, 5, 2, 6, 3, 7, 4};
 
     }
 }
