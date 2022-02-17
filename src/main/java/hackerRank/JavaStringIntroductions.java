73e7bc55397147d874c575866b955ea5bea1e605
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-02-17 21:08:49 +0900

    gooood!

---
+++ src/main/java/hackerRank/JavaStringIntroductions.java
@@ -0,0 +1,27 @@
+package hackerRank;
+
+import java.io.*;
+import java.util.*;
+
+public class JavaStringIntroductions {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String[] aArr=A.split("");
+        for(int i=0;i<aArr.length;i++){
+            if(!aArr[i].equals(aArr[aArr.length-1-i])){
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+        return;
+
+    }
+}
+
+
+
