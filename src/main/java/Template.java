51869ad621fc2ee8318ae8ec989e3631beb0d7c5
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-03-23 23:47:28 +0900

    fix : template update

--- src/main/java/Template.java
+++ src/main/java/Template.java
@@ -9,19 +9,6 @@ public class Template {
     }
 
     public static int[] solution(int[] array, int[][] commands) {
-        int i=0;
-        var a=0;
-        List<Character> a;
-        while(i<10){
-            try {
-                System.out.println("try block");
-                System.out.println(1/0);
-                break;
-            }catch (Exception e){
-                System.out.println("Catch block");
-                i++;
-            }
-        }
 
         return new int[]{1, 5, 2, 6, 3, 7, 4};
 
